b621d18cfd03133b969f6290223182c4e2f6b84b
Jan Schneider <jan.schneider@iff.csic.es>
2023-12-05 18:14:36 +0100

    format test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,10 @@ using Test;
         J_coupling = rand(Float64, N) * J
         J_coupling[end] = 0
         Γ_coupling = rand(Float64, N) * Γ
-        offdiagonal = [reduce(vcat, [-Γ_coupling[idx], J_coupling[idx]] for idx in 1:N-1); -Γ_coupling[end]]
+        offdiagonal = [
+            reduce(vcat, [-Γ_coupling[idx], J_coupling[idx]] for idx = 1:N-1)
+            -Γ_coupling[end]
+        ]
         h_free = Tridiagonal(-offdiagonal, zeros(2N), offdiagonal)
         h_d, O_free = F_utilities.Diag_real_skew(h_free, 0)
         free_modes = diag(h_d, 1)[begin:2:end]  # Biggest to smallest convention
@@ -20,12 +23,12 @@ using Test;
 end
 
 @testset "Hopping model" begin
-    N = 128;
-    H   = F_utilities.Build_hopping_hamiltonian(N,PBC=true);
-    U_ω = F_utilities.Build_Fourier_matrix(N);
-    D_Fourier = U_ω'*H*U_ω;
-    D,U = F_utilities.Diag_h(H);
+    N = 128
+    H = F_utilities.Build_hopping_hamiltonian(N, PBC = true)
+    U_ω = F_utilities.Build_Fourier_matrix(N)
+    D_Fourier = U_ω' * H * U_ω
+    D, U = F_utilities.Diag_h(H)
     @testset "Energies" begin
-        @test all(sort(diag(real.(D_Fourier))).-sort(real.(diag(D))) .< 10^(-12));
+        @test all(sort(diag(real.(D_Fourier))) .- sort(real.(diag(D))) .< 10^(-12))
     end
 end
